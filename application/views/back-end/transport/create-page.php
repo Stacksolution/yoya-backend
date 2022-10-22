02c7a7944963f34dfab457e1b0e03ff727bc11b7
Devrajput2101 <64140740+Devrajput2101@users.noreply.github.com>
2022-10-22 16:05:55 +0530

    Add files via upload
    transport

---
+++ application/views/back-end/transport/create-page.php
@@ -0,0 +1,150 @@
+<?php include(__DIR__.'/../common/_header.php'); ?>
+<?php include(__DIR__.'/../common/_sidebar.php'); ?>
+<div class="page-content">
+   <div class="container-fluid">
+      <div class="row">
+         <?php include(__DIR__.'/../common/_message.php'); ?>
+         <div class="col-12">
+            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+               <h4 class="mb-sm-0 font-size-18">Vehicle Transport Fare</h4>
+               <div class="page-title-right">
+                  <ol class="breadcrumb m-0">
+                     <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
+                     <li class="breadcrumb-item active">Vehicle Transport Fare Create</li>
+                  </ol>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-12">
+            <div class="card">
+               <div class="card-body">
+                  <div class="row mb-4">
+                     <div class="col-md-8">
+                        <h4 class="card-title">Vehicle Transport Fare Create</h4>
+                     </div>
+                     <div class="col-md-4 text-right">
+                        <div class="card-footer bg-transparent" style="margin-top: -15px;">
+                           <div class="text-center">
+                              <a href="<?= site_url('admin/transport') ?>" class="btn btn-outline-dark btn-sm align-middle me-2" title="Pages" style="float: right;">
+                              <i class="bx bx-arrow-back"></i>Vehicle Transport fare
+                              </a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <?= form_open() ?>
+                  <div class="row mb-2 mt-2">
+                     <div class="col-md-6">
+                        <label>Select Country</label>
+                        <div class="input-group" id="country">
+                           <?= form_dropdown('country_id',$country,set_value('country_id'),'class="form-control" id="country_id"') ?>
+                        </div>
+                        <?= form_error('country_id', '<div class="error">', '</div>'); ?>
+                     </div>
+                     <div class="col-md-6">
+                        <label>Select State</label>
+                        <div class="input-group" id="state">
+                           <?= form_dropdown('state_id',[''=>'--Select State--'],set_value('state_id'),'class="form-control" id="state_id" placeholder="Select State"') ?>
+                        </div>
+                        <?= form_error('state_id', '<div class="error">', '</div>'); ?>
+                     </div>
+                  </div>
+                  <div class="row mb-2 mt-2">
+                     <div class="col-md-6">
+                        <label>Select City</label>
+                        <div class="input-group" id="city_id">
+                           <?= form_dropdown('city_id',[''=>'--Select City--'],set_value('city_id'),'class="form-control" id="city_id" placeholder="Select city" ') ?>
+                        </div>
+                        <?= form_error('city_id', '<div class="error">', '</div>'); ?>
+                     </div>
+                     <div class="col-md-6">
+                        <label>Select Vehicle</label>
+                        <div class="input-group" id="vehicle_id">
+                           <?= form_dropdown('vehicle_id',$vehicles,set_value('vehicle_id'),'class="form-control"') ?>
+                        </div>
+                        <?= form_error('vehicle_id', '<div class="error">', '</div>'); ?>
+                     </div>
+                  </div>
+                  <div class="row mb-2 mt-2">
+                     <div class="col-md-6">
+                        <label>Per Minutes</label>
+                        <div class="input-group" id="fare_base_price">
+                           <input type="text" class="form-control" placeholder="base price" name="fare_base_price" value="<?= set_value('fare_base_price') ?>">
+                        </div>
+                        <?= form_error('fare_base_price', '<div class="error">', '</div>'); ?>
+                     </div>
+                     <div class="col-md-6">
+                        <label>General Price</label>
+                        <div class="input-group" id="fare_general_price">
+                           <input type="text" class="form-control" placeholder="general price" name="fare_general_price" value="<?= set_value('fare_general_price') ?>">
+                        </div>
+                        <?= form_error('fare_general_price', '<div class="error">', '</div>'); ?>
+                     </div>
+                  </div>
+                  <div class="row mb-2 mt-2">
+                     <div class="col-md-6">
+                        <label>Fare Time Free</label>
+                        <div class="input-group" id="fare_time_free">
+                           <input type="text" class="form-control" placeholder="Fare Time Free" name="fare_time_free" value="<?= set_value('fare_time_free') ?>">
+                        </div>
+                        <?= form_error('fare_time_free', '<div class="error">', '</div>'); ?>
+                     </div>
+                     <div class="col-md-6">
+                        <label>Fare Business Price</label>
+                        <div class="input-group" id="fare_business_price">
+                           <input type="text" class="form-control" placeholder="Per Minutes" name="fare_business_price" value="<?= set_value('fare_business_price') ?>">
+                        </div>
+                        <?= form_error('fare_business_price', '<div class="error">', '</div>'); ?>
+                     </div>
+                  </div>
+                  <div class="row mb-2 mt-2">
+                     <div class="col-md-6">
+                        <label>Fare Night Price</label>
+                        <div class="input-group" id="fare_night_price">
+                           <input type="text" class="form-control" placeholder="Per Minutes" name="fare_night_price" value="<?= set_value('fare_night_price') ?>">
+                        </div>
+                        <?= form_error('fare_night_price', '<div class="error">', '</div>'); ?>
+                     </div>
+                     <div class="col-md-6">
+                        <label>Fare Business Price</label>
+                        <div class="input-group" id="fare_extra_waiting_price">
+                           <input type="text" class="form-control" placeholder="Fare Extra Waiting Price" name="fare_extra_waiting_price" value="<?= set_value('fare_extra_waiting_price') ?>">
+                        </div>
+                        <?= form_error('fare_extra_waiting_price', '<div class="error">', '</div>'); ?>
+                     </div>
+                  </div>
+                  <div class="row mb-2 mt-2">
+                     <div class="col-md-6">
+                        <label>Fare Stop Price</label>
+                        <div class="input-group" id="fare_stop_price">
+                           <input type="text" class="form-control" placeholder="Fare Stop Price" name="fare_stop_price" value="<?= set_value('fare_stop_price') ?>">
+                        </div>
+                        <?= form_error('fare_stop_price', '<div class="error">', '</div>'); ?>
+                     </div>
+                     <div class="col-md-6">
+                        <label>Fare commission Price</label>
+                        <div class="input-group" id="fare_commission">
+                           <input type="text" class="form-control" placeholder="Fare commission Price" name="fare_commission" value="<?= set_value('fare_night_price') ?>">
+                        </div>
+                        <?= form_error('fare_commission', '<div class="error">', '</div>'); ?>
+                     </div>
+                  </div>
+                  <div class="row mt-4">
+                     <div class="col-md-6">
+                        <button type="submit" class="btn btn-primary waves-effect waves-light">
+                        <i class="bx bx-save font-size-16 align-middle me-2"></i>submit
+                        </button>
+                     </div>
+                  </div>
+                  <?= form_close() ?>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- End Page-content -->
+<?php include(__DIR__.'/../common/_footer.php'); ?>
+<?php include(__DIR__.'/../common/_get_dependent_location.php'); ?>
--- application/views/back-end/transport/index-page.php
+++ application/views/back-end/transport/index-page.php
@@ -21,19 +21,19 @@
             <div class="card">
                <div class="card-body">
                   <div class="row mb-4">
-                    <div class="col-md-8">
+                     <div class="col-md-8">
                         <h4 class="card-title">Vehicle Transport Fare</h4>
-                    </div>
+                     </div>
                      <div class="col-md-4 text-right">
                         <div class="card-footer bg-transparent" style="margin-top: -15px;">
-                            <div class="text-center">
-                                <a href="<?= site_url('admin/transport/create') ?>" class="btn btn-outline-success btn-sm align-middle me-2" title="Vehicle" style="float: right;">
-                                    <i class="fas fa-plus"></i> New Transport fare
-                                </a>
-                            </div>
+                           <div class="text-center">
+                              <a href="<?= site_url('admin/transport/create') ?>" class="btn btn-outline-success btn-sm align-middle me-2" title="Vehicle" style="float: right;">
+                              <i class="fas fa-plus"></i> New Transport fare
+                              </a>
+                           </div>
                         </div>
-                    </div>
-                </div>
+                     </div>
+                  </div>
                   <div class="table-responsive">
                      <table id="datatable-buttons" class="table table-bordered dt-responsive nowrap w-100">
                         <thead class="table-light">
@@ -45,11 +45,16 @@
                               <th class="align-middle">Vehicle</th>
                               <th class="align-middle">Base Price</th>
                               <th class="align-middle">General Price</th>
+                              <th class="align-middle">Business Price</th>
+                              <th class="align-middle">Fare Night Price</th>
+                              <th class="align-middle">Fare Extra Waiting Price</th>
+                              <th class="align-middle">Stop Price</th>
+                              <th class="align-middle">Fare Commission</th>
                               <th class="align-middle">Action</th>
                            </tr>
                         </thead>
                         <tbody>
-                           <?php foreach($rentalfares->result() as $key => $data) { ?>
+                           <?php foreach($transport->result() as $key => $data) { ?>
                            <tr>
                               <td><a href="javascript: void(0);" class="text-body fw-bold">#<?= $key + 1 ?></a> </td>
                               <td><?= $data->country_name ?></td>
@@ -58,10 +63,15 @@
                               <td><?= $data->vehicle_name ?></td>
                               <td><?= $data->fare_base_price?></td>
                               <td><?= $data->fare_general_price ?></td>
+                              <td><?= $data->fare_business_price ?></td>
+                              <td><?= $data->fare_night_price ?></td>
+                              <td><?= $data->fare_extra_waiting_price ?></td>
+                              <td><?= $data->fare_stop_price ?></td>
+                              <td><?= $data->fare_commission ?></td>
                               <td>
                                  <div class="d-flex gap-3">
-                                    <a href="<?= site_url('admin/rental/update/'.$data->fare_id) ?>" class="btn btn-outline-secondary btn-sm"><i class="mdi mdi-pencil font-size-13"></i></a>
-                                    <a href="<?= site_url('admin/rental/remove/'.$data->fare_id) ?>" class="btn btn-outline-danger btn-sm" onclick="return confirm('Are you sure you want to remove this fare ?');"><i class="mdi mdi-delete font-size-13"></i></a>
+                                    <a href="<?= site_url('admin/transport/update/'.$data->fare_id) ?>" class="btn btn-outline-secondary btn-sm"><i class="mdi mdi-pencil font-size-13"></i></a>
+                                    <a href="<?= site_url('admin/transport/remove/'.$data->fare_id) ?>" class="btn btn-outline-danger btn-sm" onclick="return confirm('Are you sure you want to remove this fare ?');"><i class="mdi mdi-delete font-size-13"></i></a>
                                  </div>
                               </td>
                            </tr>
