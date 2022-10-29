77a462c9e21e61e7a7ba92297778fe0383e803a3
Devrajput2101 <64140740+Devrajput2101@users.noreply.github.com>
2022-10-29 12:32:06 +0530

    Update cancel-create.php

--- application/views/back-end/systemconfig/cancel-create.php
+++ application/views/back-end/systemconfig/cancel-create.php
@@ -6,11 +6,11 @@
          <?php include(__DIR__.'/../common/_message.php'); ?>
          <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-               <h4 class="mb-sm-0 font-size-18">Cancellation Charges Updated</h4>
+               <h4 class="mb-sm-0 font-size-18">Cancellation Charges Create</h4>
                <div class="page-title-right">
                   <ol class="breadcrumb m-0">
                      <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                     <li class="breadcrumb-item active">Cancellation Charges Updated
+                     <li class="breadcrumb-item active">Cancellation Charges Create
                </div>
             </div>
          </div>
@@ -21,13 +21,13 @@
                <div class="card-body">
                   <div class="row mb-4">
                      <div class="col-md-8">
-                        <h4 class="card-title">Cancellation Charges Updated</h4>
+                        <h4 class="card-title">Cancellation Charges Create</h4>
                      </div>
                      <div class="col-md-4 text-right">
                         <div class="card-footer bg-transparent" style="margin-top: -15px;">
                            <div class="text-center">
                               <a href="<?= site_url('admin/cancelation') ?>" class="btn btn-outline-dark btn-sm align-middle me-2" title="Pages" style="float: right;">
-                              <i class="bx bx-arrow-back"></i>Cancellation Charges Updated
+                              <i class="bx bx-arrow-back"></i>Cancellation Charges Create
                               </a>
                            </div>
                         </div>
