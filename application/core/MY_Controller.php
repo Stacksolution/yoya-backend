d5b40cc6340b44f05c37664a2825c4996b10a576
Devrajput2101 <64140740+Devrajput2101@users.noreply.github.com>
2022-10-29 12:07:07 +0530

    Update MY_Controller.php

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -158,6 +158,7 @@ class MY_AdminController extends MY_Controller {
         $this->load->model('admin-models/RentalPakageModel');
         $this->load->model('admin-models/RentalFareModel');
 	$this->load->model('admin-models/TransportfareModel');
+        $this->load->model('admin-models/CancelationModel');
     }
 }
 
